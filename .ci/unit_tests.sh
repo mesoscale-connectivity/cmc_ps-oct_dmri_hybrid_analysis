39bd9188e30d4f7e2bbc3d13e85ded9db11d4b9a
Saad Jbabdi <saad.jbabdi@ndcn.ox.ac.uk>
2025-10-01 13:49:47 +0100

    Edit unit_tests.sh

--- .ci/unit_tests.sh
+++ .ci/unit_tests.sh
@@ -1,13 +1,11 @@
 set -e
 
+projroot=$(cd $(dirname $0) && pwd)/..
 conda create \
-        -c https://fsl.fmrib.ox.ac.uk/fsldownloads/fslconda/public/ \
-        -c conda-forge \
-        -p /fsl \
-        fsl-avwutils fsl-flirt \
-        python pytest
+  -c https://fsl.fmrib.ox.ac.uk/fsldownloads/fslconda/public/ \
+  -c conda-forge \
+  -p /fsl \
+  fsl-avwutils fsl-flirt \
+  python $(cat ${projroot}/requirements.txt)
 source activate /fsl
-pip install . pytest
-export FSLDIR=/fsl
-source $FSLDIR/etc/fslconf/fsl.sh
-pytest -v .
+pip install .
