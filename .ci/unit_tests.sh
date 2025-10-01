28f978956fddfeb0f235244de1fdaab3f4da1c70
Saad Jbabdi <saad@winpd0051-2.local>
2025-10-01 10:39:43 +0100

    CI/CD script

---
+++ .ci/unit_tests.sh
@@ -0,0 +1,11 @@
+conda create \
+        -c https://fsl.fmrib.ox.ac.uk/fsldownloads/fslconda/public/ \
+        -c conda-forge \
+        -p /fsl \
+        fsl-avwutils fsl-flirt \
+        python pytest
+conda activate /fsl
+pip install . pytest
+export FSLDIR=/fsl
+source $FSLDIR/etc/fslconf/fsl.sh
+pytest -v .
