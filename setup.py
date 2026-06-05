462262a3c80846f8266ee558b3fd57ba48279eec
Vasilis Karlaftis <vasilis.karlaftis@ndcn.ox.ac.uk>
2026-06-05 12:59:08 +0100

    fix setup errors

--- setup.py
+++ setup.py
@@ -18,14 +18,18 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    packages=['cmc_hybrid',],
+    packages=['cmc_hybrid', 'cmc_hybrid.scripts'],
     install_requires=install_requires,
     extras_require={
         "dev": ["pytest",
                 "flake8"],
     },
 
-    scripts=['cmc_hybrid/scripts/cmc_hybrid.py',
-             'cmc_hybrid/scripts/cmc_slice_mask.py',
-             'cmc_hybrid/scripts/cmc_bpx2fod.py']
+    entry_points={
+        'console_scripts': [
+            'cmc_hybrid = cmc_hybrid.scripts.cmc_hybrid:main',
+            'cmc_slice_mask = cmc_hybrid.scripts.cmc_slice_mask:main',
+            'cmc_bpx2fod = cmc_hybrid.scripts.cmc_bpx2fod:main',
+        ],
+    }
 )
